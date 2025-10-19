8a1123caf8a49ed640be70704db5c0aac378d370
Oleksandr Mazur <mazur.ol.sr@gmail.com>
2025-10-19 16:26:56 +0300

    add snowflake connector installation

--- dbt-snowplow-web/snowflake/dbt.sh
+++ dbt-snowplow-web/snowflake/dbt.sh
@@ -5,5 +5,6 @@ python3 -m venv env
 source env/bin/activate
 python3 -m pip install --upgrade pip >/dev/null 2>&1
 python3 -m pip install dbt-core==1.9.8 dbt-snowflake==1.9.1
+python3 -m pip install snowflake-connector-python>=3.0.0
 
 
