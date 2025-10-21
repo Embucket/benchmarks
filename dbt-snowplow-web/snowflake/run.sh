47a1401c9330f30445509a234d5771623c8f731f
Oleksandr Mazur <mazur.ol.sr@gmail.com>
2025-10-21 19:07:18 +0300

    dbt snowplow data fix

--- dbt-snowplow-web/snowflake/run.sh
+++ dbt-snowplow-web/snowflake/run.sh
@@ -25,4 +25,4 @@ dbt debug
 dbt clean
 dbt deps
 dbt seed
-dbt run --vars '{snowplow__enable_consent: true, snowplow__enable_cwv: true, snowplow__enable_iab: true, snowplow__enable_ua: true, snowplow__enable_yauaa: true}'  
+dbt run --vars '{snowplow__enable_consent: true, snowplow__enable_cwv: true, snowplow__enable_iab: true, snowplow__enable_ua: true, snowplow__enable_yauaa: true, snowplow__start_date: '2025-10-01', snowplow__backfill_limit_days: 50, snowplow__cwv_days_to_measure: 999}'  
